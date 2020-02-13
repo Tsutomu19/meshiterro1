dc34af1d274912c3a6650b58d9d906a67f51ffa8
竹田勤 <takedatsutomu@taketakinnoMacBook-puro.local>
2020-02-13 15:19:03 +0900

    [Add]5-3_ログイン画面、一覧画面、詳細画面_作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'af1b2dce45354c68f93661f3fecd2711824a27c79ca61d5b330721eec4d8720f4b0fab49096c4bcaf49e3b5be9ff1039ba7ad39327ab6fa7972d4134a2ba9467'
